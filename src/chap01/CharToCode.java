c327c6e48bf754985578ba821cc58b3472dadf59
YuHyeonKwon <129023192+YuHyeonKwon@users.noreply.github.com>
2023-03-27 13:36:11 +0900

    Update CharToCode.java

--- src/chap01/CharToCode.java
+++ src/chap01/CharToCode.java
@@ -10,6 +10,7 @@ public class CharToCode { // class s
 		char characteC = 'a';
 		int charCode = (int)characteC;
 		System.out.printf("%c = %d",characteC,charCode);
+		System.out.printf("%c = %d",characteC,charCode);
 	} // main e
 
 } // class e
